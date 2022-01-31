a0380115f75f5db99aaa49609411abc7c36dc71f
Gurunandan Rao <gurunandan.rao@oracle.com>
2022-01-31 11:00:42 +0530

    glassfish7, jdk17, fix for userguide compilation error with jdk11.
    Signed-off-by: Gurunandan Rao <gurunandan.rao@oracle.com>

--- docker/run_dsoltck.sh
+++ docker/run_dsoltck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 #
-# Copyright (c) 2019, 2021 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2019, 2022 Oracle and/or its affiliates. All rights reserved.
 #
 # This program and the accompanying materials are made available under the
 # terms of the Eclipse Public License v. 2.0, which is available at
@@ -34,7 +34,7 @@ cd $WORKSPACE
 export GF_HOME="${WORKSPACE}"
 
 if [ -z "$GF_TOPLEVEL_DIR" ]; then
-  export GF_TOPLEVEL_DIR=glassfish6
+  export GF_TOPLEVEL_DIR=glassfish7
 fi
 
 WGET_PROPS="--progress=bar:force --no-cache"
